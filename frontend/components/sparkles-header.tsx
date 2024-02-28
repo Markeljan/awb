f5480d05eeecdc54e6ad58ba4e8264951d329697
Markeljan Sokoli <Markeljan19@gmail.com>
2024-02-27 20:54:08 -0700

    frontend dir

--- frontend/components/sparkles-header.tsx
+++ frontend/components/sparkles-header.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { SparklesCore } from "@/components/ui/sparkles";
+import { SparklesCore } from "@/frontend/components/ui/sparkles";
 
 export function SparklesPreview() {
   return (
