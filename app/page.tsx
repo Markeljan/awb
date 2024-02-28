469da83309d24ebb6741858efe8d0dab9bd28aeb
Markeljan Sokoli <Markeljan19@gmail.com>
2024-02-27 20:52:30 -0700

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { SparklesPreview } from "@/components/sparkles-header";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <SparklesPreview />
+    </main>
+  );
+}
